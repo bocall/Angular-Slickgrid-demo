5173d649eb1e809827ac950f755b5cad4bab7469
Ghislain Beaulac <Ghislain.Beaulac@schneider-electric.com>
2018-06-15 18:37:31 -0400

    update to latest version 1.0.19

--- src/app/examples/grid-graphql.component.ts
+++ src/app/examples/grid-graphql.component.ts
@@ -1,4 +1,3 @@
-import { Subscription } from 'rxjs/Subscription';
 import { Component, Injectable, OnInit, OnDestroy } from '@angular/core';
 import { TranslateService } from '@ngx-translate/core';
 import {
@@ -14,6 +13,7 @@ import {
   OperatorType,
   SortDirection
 } from 'angular-slickgrid';
+import { Subscription } from 'rxjs/Subscription';
 
 const defaultPageSize = 20;
 const GRAPHQL_QUERY_DATASET_NAME = 'users';
